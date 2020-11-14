fef2f88c8c5ca14e5e5b2e7d0dac8f18537cc154
Alberto Gotta <gotta.alberto@gmail.com>
2020-11-14 18:10:19 +0100

    Correcting last commit - Refactoring custom iterator class

--- CustomIterator.h
+++ CustomIterator.h
@@ -1,18 +1,81 @@
+// Forward declaration for template friend operator
+template <class T> class CustomIterator;
+template <class T> std::ostream &operator<<(std::ostream &str, const CustomIterator<T> &it);
+
 template <class T>
 class CustomIterator
 {
 public:
-    CustomIterator(T *ptr) : m_ptr(ptr){}
-    inline bool operator==(const CustomIterator<T> &other){return m_ptr == other.m_ptr;}
-    inline bool operator!=(const CustomIterator<T> &other){return m_ptr != other.m_ptr;}
-    inline bool hasNext(){return m_ptr->hasNext();}
-    inline CustomIterator<T> &next(){if(m_ptr->hasNext()) m_ptr = m_ptr->next(); return (*this);}
-    inline CustomIterator<T> &operator++(){return next();}
-    inline CustomIterator<T> operator++(int){CustomIterator<T> tmp = CustomIterator<T>(m_ptr); m_ptr = m_ptr->next(); return tmp;}
-    // inline CustomIterator<T> &operator--(){--m_ptr; return (*this);}
-    inline T operator*(){return *m_ptr;}
-    friend std::ostream &operator<<(std::ostream &str, const CustomIterator<T> &it){str << it.m_ptr; return str;}
+                            CustomIterator  (T *ptr);
+    bool                    operator==      (const CustomIterator<T> &other);
+    bool                    operator!=      (const CustomIterator<T> &other);
+    bool                    hasNext         ();
+    CustomIterator<T>      &next            ();
+    CustomIterator<T>      &operator++      ();
+    CustomIterator<T>       operator++      (int);
+    T                       operator*       ();
+    friend std::ostream    &operator<< <>   (std::ostream &str, const CustomIterator<T> &it);
 
 private:
     T *m_ptr;
 };
+
+template <class T>
+CustomIterator<T>::CustomIterator(T *ptr) : m_ptr(ptr)
+{
+}
+
+template <class T>
+bool CustomIterator<T>::operator==(const CustomIterator<T> &other)
+{
+    return m_ptr == other.m_ptr;
+}
+
+template <class T>
+bool CustomIterator<T>::operator!=(const CustomIterator<T> &other)
+{
+    return m_ptr != other.m_ptr;
+}
+
+template <class T>
+bool CustomIterator<T>::hasNext()
+{
+    return m_ptr->hasNext();
+}
+
+template <class T>
+CustomIterator<T> &CustomIterator<T>::next()
+{
+    if(m_ptr->hasNext()) 
+        m_ptr = m_ptr->next(); 
+    
+    return (*this);
+}
+
+template <class T>
+CustomIterator<T> &CustomIterator<T>::operator++()
+{
+    return next();
+}
+
+template <class T>
+CustomIterator<T> CustomIterator<T>::operator++(int)
+{
+    CustomIterator<T> tmp = CustomIterator<T>(m_ptr); 
+    m_ptr = m_ptr->next(); 
+    
+    return tmp;
+}
+
+template <class T>
+T CustomIterator<T>::operator*()
+{
+    return *m_ptr;
+}
+
+template <class T>
+std::ostream &operator<<(std::ostream &str, const CustomIterator<T> &it)
+{
+    str << it.m_ptr; 
+    return str;
+}
