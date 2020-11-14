4339cb3f6ea83bd7ed14eb4ff1c5313b2866fd61
Alberto Gotta <gotta.alberto@gmail.com>
2020-11-14 01:09:42 +0100

    Added multiple data types example for GenericContainer

--- Tests/GenericContainerTest.cpp
+++ Tests/GenericContainerTest.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "../GenericContainer.h"
 
+struct Key1;
+struct Key2;
+
 int main()
 {
     GenericContainer<Slot<int>, Slot<std::string>> container;
@@ -10,4 +13,11 @@ int main()
 
     std::cout << container.get<int>() << std::endl;
     std::cout << container.get<std::string>() << std::endl;
+
+    GenericContainer<Slot<std::string, Key1>, Slot<std::string, Key2>> MultipleContainer;
+    MultipleContainer.set<std::string, Key1>("Foo");
+    MultipleContainer.set<std::string, Key2>("Bar");
+
+    std::cout << MultipleContainer.get<std::string, Key2>() << std::endl;
+    std::cout << MultipleContainer.get<std::string, Key1>() << std::endl;
 }
